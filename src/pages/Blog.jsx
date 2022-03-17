d0f6dc5412a88475977e53467f9a3c669ee3fbd3
Sergio99778 <sergio4brandon@gmail.com>
2022-03-17 02:43:02 -0600

    Adding hosting url images to Blog.jsx

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,37 +1,54 @@
-import React from 'react';
-import Projects from '../Components/Projects';
-
-//Images
-import prove from '../img/404.svg';
-import clandestino from '../img/projects/clandestino.jpg';
-import landing from '../img/projects/landing-page.jpg';
-import places from '../img/projects/places.jpg';
-import tribute from '../img/projects/tribute.jpg';
+import React from "react";
+import Projects from "../Components/Projects";
 
 const data = [
   {
-    name: 'Clandestino',
-    url: 'https://github.com/Sergio99778/clandestino',
-    img: clandestino,
-    desc: 'Clandestino project',
+    name: "Clandestino",
+    url: "https://github.com/Sergio99778/clandestino",
+    img: "https://i.ibb.co/qxXfw6y/clandestino.jpg",
+    desc: "Clandestino project",
   },
   {
-    name: 'Landing Page',
-    url: 'https://github.com/Sergio99778/places-react',
-    img: landing,
-    desc: 'Landing project',
+    name: "Places",
+    url: "https://github.com/Sergio99778/places-react",
+    img: "https://i.ibb.co/jMxDvtb/places.jpg",
+    desc: "Places project",
   },
   {
-    name: 'Places',
-    url: 'https://github.com/Sergio99778/places-react',
-    img: places,
-    desc: 'Places project',
+    name: "Appointment",
+    url: "https://citas-gamma.vercel.app/",
+    img: "https://i.ibb.co/9hSkpWw/prj1.png",
+    desc: "Appointment manager",
   },
   {
-    name: 'Tribute',
-    url: 'https://sergio99778.github.io/responsive-freeCodeCamp/Build-a-Tribute-Page/index.html',
-    img: tribute,
-    desc: 'Tribute project',
+    name: "Budget",
+    url: "https://budget-xi.vercel.app/",
+    img: "https://i.ibb.co/r6hFP2z/Screenshot-at-2022-03-17-02-27-17-1.png",
+    desc: "Budget manager",
+  },
+  {
+    name: "Insurance quote",
+    url: "https://insurance-quote-seven.vercel.app/",
+    img: "https://i.ibb.co/W5xP6Qk/Screenshot-at-2022-03-17-02-27-49-1.png",
+    desc: "Insurance quote",
+  },
+  {
+    name: "Cotize your Crypto",
+    url: "https://crypto-app-vert.vercel.app/",
+    img: "https://i.ibb.co/mhpdMsj/Screenshot-at-2022-03-17-02-28-24-1.png",
+    desc: "Cryto cotizer",
+  },
+  {
+    name: "Landing Page",
+    url: "https://github.com/Sergio99778/places-react",
+    img: "https://i.ibb.co/86175JY/landing-page.jpg",
+    desc: "Landing project",
+  },
+  {
+    name: "Tribute",
+    url: "https://sergio99778.github.io/responsive-freeCodeCamp/Build-a-Tribute-Page/index.html",
+    img: "https://i.ibb.co/WzYYQNH/tribute.jpg",
+    desc: "Tribute project",
   },
 ];
 
